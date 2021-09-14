377fa56ad87617e2b69738f89605bc8b21aac8e3
chadmuro <chadmuro@gmail.com>
2021-09-14 23:06:37 +0900

    add papa-parse

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
-import { useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
+import Papa from 'papaparse';
 
 function App() {
+  const [parsedCsvData, setParsedCsvData] = useState([]);
+
+  const parseFile = file => {
+    Papa.parse(file, {
+      header: true,
+      complete: async results => {
+        setParsedCsvData(results.data);
+      },
+    });
+  };
+
+  console.log(parsedCsvData);
+
   const onDrop = useCallback(acceptedFiles => {
-    console.log(acceptedFiles);
+    if (acceptedFiles.length) {
+      parseFile(acceptedFiles[0]);
+    }
   }, []);
 
   const {
@@ -35,6 +51,10 @@ function App() {
         )}
       </div>
       <button disabled>Import</button>
+      <div>
+        {parsedCsvData &&
+          parsedCsvData.map(parsedData => <p>{parsedData.Kanji}</p>)}
+      </div>
     </div>
   );
 }
